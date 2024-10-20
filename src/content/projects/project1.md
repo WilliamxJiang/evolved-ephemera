d09e218b321378d9742eecac18852a85b7363d69
WilliamxJiang <williamjiang021@gmail.com>
2024-10-19 21:08:58 -0400

    basic draft of site

---
+++ src/content/projects/project1.md
@@ -0,0 +1,6 @@
+---
+title: "10/19/24"
+description: "Launched this site so my domain name doesn't turn inactive haha..."
+---
+
+Here is content for Project 1
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Add this line to enable class-based dark mode
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
